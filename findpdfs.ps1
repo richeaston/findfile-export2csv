95e974b89d6bb603b23972396d06635d7109796f
Richard Easton <richeaston@users.noreply.github.com>
2020-05-18 12:06:25 +0100

    Create findpdfs.ps1

---
+++ findpdfs.ps1
@@ -0,0 +1,19 @@
+$array = @()
+
+$files = Get-ChildItem -Filter "*.pdf" -Path %systemroot -Recurse | select Name, Fullname, Lastaccesstime
+
+foreach ($file in $files) {
+    $acl = get-acl $file.fullname | Select -ExpandProperty Owner
+    
+    $output = [PScustomobject]@{
+        acl = $acl
+        name = $file.Name
+        path = $file.FullName
+        lastaccessed = $file.LastAccessTime
+    }
+    $array += $output
+}
+
+$array | Sort-object Name
+
+$array | Export-Csv "D:\pdfs.csv" -force -NoClobber -NoTypeInformation
